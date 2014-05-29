d93fe147fcf347216a9d610f33018a156f2d91db
Sebastian Krüger <krueger@secra.de>
2014-05-29 23:26:46 +0200

    Start module config and save player name and number

---
+++ index.js
@@ -0,0 +1,13 @@
+/*------------------------------------------------------------------------------
+
+ Project : baseball-stats
+ Filename : index.js
+ Autor : (c) Sebastian Krüger <krueger.oh@googlemail.com>
+ Date : 29.05.2014
+
+ For the full copyright and license information, please view the LICENSE
+ file that was distributed with this source code.
+
+ ----------------------------------------------------------------------------*/
+
+exports.BaseballStats = require('./lib/baseball-stats').BaseballStats;
