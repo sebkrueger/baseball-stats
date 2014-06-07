7b1f0101f6bef9e8c7463d8e48541035fd4bf645
Sebastian Krüger <krueger.oh@googlemail.com>
2014-06-07 04:26:26 +0200

    Rearrange tests

--- test/baseball-stats.js
+++ test/baseball-stats.js
@@ -1,17 +1,21 @@
 var assert = require('assert');
 var BaseballStats = require('../index').BaseballStats;
 
-describe('BaseballStats', function() {
+describe('BaseballStats', function()
+{
+    describe('#getPlayerNumber()', function()
+    {
+        var stats = new BaseballStats('4','John Doe');
 
-    var stats = new BaseballStats('4','John Doe');
-
-    describe('#getPlayerNumber()', function(){
         it('should return player number', function(){
             assert.equal('4', stats.getPlayerNumber());
         });
     });
 
-    describe('#getPlayerName()', function(){
+    describe('#getPlayerName()', function()
+    {
+        var stats = new BaseballStats('4','John Doe');
+
         it('should return player name', function(){
             assert.equal('John Doe', stats.getPlayerName());
         });
