56b2b4e4240be8477d295288ed94534bee9b37c9
Sebastian Krüger <krueger.oh@googlemail.com>
2014-06-07 04:29:55 +0200

    basic example

---
+++ example/baseball.js
@@ -0,0 +1,10 @@
+/**
+ * use the baseball-stats module
+ */
+
+// get the module
+var baseballStats = require('../').BaseballStats;
+
+// Start with Player John Doe he has jersy number #4
+var stats = new baseballStats('4','John Doe');
+
